d8674b9d094a0ec330f3130f573db732b6873bba
Rafael Gomez <gomezrafaelantonio@gmail.com>
2016-10-11 03:53:19 -0500

    Cambio en actualizacion de la serpiente y colision con los lados arriba, abajo

--- Snake.asm
+++ Snake.asm
@@ -3,8 +3,8 @@
 org 100h
 
 mov ch, 32          ;Cambia el tipo de cursor
-mov ah, 1
-int 10h             ;Esto es por probar 
+mov ah, 1           ;
+int 10h             ;
 call caracter
 
 inicio:   
@@ -12,7 +12,12 @@ inicio:
     mov color, 10   ;cambia el color a verde para culebrita
     
     mov ah, 0       ;
-    int 16h         ;interrupcion leer tecla                                       
+    int 16h         ;interrupcion leer tecla  
+    
+    mov al, vv1
+    mov valorv, al
+    mov al, vh1
+    mov valorh, al                                     
     
     cmp ah, 4Dh     ;comparar tecla flecha derecha
     je et1 
@@ -31,24 +36,38 @@ et5:
     jmp inicio
           
 et4:
-    add valorv,1    ;suma uno al valor vertical
+    add valorv,1    ;suma uno al valor vertical 
+    mov al, valorv
+    cmp al, 25
+    je fin
     call caracter   ;llama caracter para imprimir cambio
     jmp inicio      ;salta a inicio
               
 et3:
     sub valorh,1    ;suma uno al valor horizontal
+    mov al, valorh
+    cmp al, 80
+    je fin
     call caracter   ;llama caracter para imprimir cambio
     jmp inicio      ;salta a inicio
     
 et2:
     sub valorv,1    ;resta uno al valor vertical
+    mov al, valorv
+    cmp al, -1
+    je fin
     call caracter   ;llama caracter para imprimir cambio
     jmp inicio      ;salta a inicio
 
 et1:    
-    add valorh,1    ;resta uno al valor horizontal
+    add valorh,1    ;resta uno al valor horizontal 
+    mov al, valorh
+    cmp al, -1
+    je fin
     call caracter   ;llama caracter para imprimir cambio
-    jmp inicio      ;salta a inicio
+    jmp inicio      ;salta a inicio 
+    
+fin:
 ret 
 
 actual:
@@ -64,16 +83,12 @@ actual:
    mov vv2, al
    mov al, vh1
    mov vh2, al   
-   mov al, columna
-   mov vh1, al
    mov al, fila
    mov vv1, al
+   mov al, columna
+   mov vh1, al
    mov color, 0
    call caracter 
-   mov al, vv1
-   mov valorv, al
-   mov al, vh1
-   mov valorh, al
 ret
 
 caracter:
@@ -99,16 +114,17 @@ ret
 ;--------------------------------------------
 
 vh1 db 10
-vh2 db 20
-vh3 db 30
+vh2 db 10
+vh3 db 10
 vv1 db 10
-vv2 db 20
-vv3 db 30
+vv2 db 10
+vv3 db 10
             
-valor db 0            
+;valor db 0            
 valorv db 10
 valorh db 0 
 color db 10
-limite dw 0
+limitev dw 25
+limiteh dw 80
 fila db 10
 columna db 0
